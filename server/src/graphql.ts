782455556fa6ed8a1bfa72c114c87b5e9acdbb03
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-08-12 00:07:17 +0200

    mutation

--- server/src/graphql.ts
+++ server/src/graphql.ts
@@ -37,7 +37,19 @@ const query = new GraphQLObjectType({
 const mutation = new GraphQLObjectType({
   name: "Mutation",
   fields: {
-    hello: { type: GraphQLString, resolve: () => "Hello from the Mutation!" }
+    deleteListing: {
+      type: GraphQLNonNull(Listing),
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) }
+      },
+      resolve: (_root, { id }) => {
+        for (let i = 0; i < listings.length; i++)
+          if (listings[i].id === id) {
+            return listings.splice(i, 1)[0];
+          }
+        throw new Error("failed to delete listings");
+      }
+    }
   }
 });
 
